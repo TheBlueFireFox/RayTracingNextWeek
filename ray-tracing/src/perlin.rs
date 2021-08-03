8d118925f2141bd350bd09add68a49e209d9f371
Adrian Hornung <hornung.adrian@gmail.com>
2021-08-03 15:35:20 +0200

    until 5.4

--- ray-tracing/src/perlin.rs
+++ ray-tracing/src/perlin.rs
@@ -20,16 +20,44 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point) -> f64 {
-         let calc = |v| {
-             let a = (4.0 * v) as isize;
-             (a & (POINT_COUNT as isize - 1)) as usize
-         };
+        // let calc = |v| {
+        //     let a = (4.0 * v) as isize;
+        //     (a & (POINT_COUNT as isize - 1)) as usize
+        // };
 
-         let i = calc(p.x());
-         let j = calc(p.y());
-         let k = calc(p.z());
+        // let i = calc(p.x());
+        // let j = calc(p.y());
+        // let k = calc(p.z());
 
-         self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        // self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let calc = |v: f64| {
+            let res = v - v.floor();
+            res*res*(3.0-2.0*res)
+        };
+
+        let u = calc(p.x());
+        let v = calc(p.y());
+        let w = calc(p.z());
+
+        let calc = |v: f64| v.floor() as isize;
+        let i = calc(p.x());
+        let j = calc(p.y());
+        let k = calc(p.z());
+        let mut c = [[[0.0;2];2];2];
+
+        let calc = |i, di| ((i + di as isize) & 255) as usize;
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.ranfloat [
+                        self.perm_x[calc(i, di)] ^
+                        self.perm_y[calc(j, dj)] ^
+                        self.perm_z[calc(k, dk)] 
+                    ];
+                }
+            }
+        }
+        Self::trilinear_interp(&c, u, v, w)
     }
 
     fn trilinear_interp(c: &[[[f64; 2]; 2]], u: f64, v: f64, w: f64) -> f64 {
